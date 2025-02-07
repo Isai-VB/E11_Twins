2a60573917e1e92ab21f2da298a8aa068b16dc66
aramvaldez <avaldez@berkeley.edu>
2025-02-07 15:50:47 -0800

    One line attempt 2

--- weather.py
+++ weather.py
@@ -8,8 +8,9 @@ bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 bme680.seal_level_pressure = 1013.25
 start_time = time.time()
 duration = 6
+t = time.localtime()
 while  time.time() - start_time < duration:
-	t = time.localtime()
+
 	current_time = time.strftime("%H:%M:%S", t)
 	print(current_time, "\nTemperature: %0.1f C" % bme680.temperature, "Gas: %d ohm" % bme680.gas, "Humidity: %0.1f %%" % bme680.relative_humidity, "Pressure: %0.3f hPa" % bme680.pressure, "Altitude = %0.2f meters" % bme680.altitude)	
 	time.sleep(2)
