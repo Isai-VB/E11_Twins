f1d06f2643b5bfeb9718aa5e4070f02211436f8a
aramvaldez <avaldez@berkeley.edu>
2025-02-07 15:39:37 -0800

    Moved whole time text to while loop

--- weather.py
+++ weather.py
@@ -6,14 +6,15 @@ i2c = board.I2C()
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
 bme680.seal_level_pressure = 1013.25
-t = time.localtime()
-current_time = time.strftime("%H:%M:%S", t)
+
 while True: 
 	print("\nTemperature: %0.1f C" % bme680.temperature)
 	print("Gas: %d ohm" % bme680.gas)
 	print("Humidity: %0.1f %%" % bme680.relative_humidity)
 	print("Pressure: %0.3f hPa" % bme680.pressure)
 	print("Altitude = %0.2f meters" % bme680.altitude)
+	t = time.localtime()
+	current_time = time.strftime("%H:%M:%S", t)
 	print(current_time)
 	
 	time.sleep(2)
