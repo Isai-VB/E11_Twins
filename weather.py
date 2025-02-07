7db8d83cb66268254d99c6a8ef0ab246c4f138ea
aramvaldez <avaldez@berkeley.edu>
2025-02-07 15:54:11 -0800

    One line attempt 5

--- weather.py
+++ weather.py
@@ -10,6 +10,7 @@ start_time = time.time()
 duration = 6
 while  time.time() - start_time < duration:
 	t = time.localtime()
+	current_time = time.strftime("%H:%M:%S", t)
 	print(current_time, "\nTemperature: %0.1f C" % bme680.temperature, "Gas: %d ohm" % bme680.gas, "Humidity: %0.1f %%" % bme680.relative_humidity, "Pressure: %0.3f hPa" % bme680.pressure, "Altitude = %0.2f meters" % bme680.altitude)	
 	time.sleep(2)
 	
