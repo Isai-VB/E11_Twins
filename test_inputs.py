546cd88017f79f64c89539d1246b7873bb4a5cfb
ABLREY <abielreyes@berkeley.edu>
2025-02-28 15:56:49 -0800

    Update test_inputs.py

--- test_inputs.py
+++ test_inputs.py
@@ -1,3 +1,4 @@
+
 import sys
 import time
 import csv
@@ -6,13 +7,15 @@ import serial
 import adafruit_bme680
 from adafruit_pm25.uart import PM25_UART
 
+ 
 print(sys.argv)
 
 if len(sys.argv) < 2:
-    print("Script requires run_time(int) as an input")
-    exit()
+  print("Script requires run_time(int) as an input")
+  exit()
 else:
-    run_time = int(sys.argv[1])
+  run_time = int(sys.argv[1])
+
 
 i2c = board.I2C()
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
@@ -22,8 +25,9 @@ uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 reset_pin = None  
 pm25 = PM25_UART(uart, reset_pin)
 
+
 # Open CSV file 
-filename = "NewSensors.csv"
+filename = "datadiff.csv"
 with open(filename, "w", newline='') as file:
     file_writer = csv.writer(file)
     
@@ -32,22 +36,22 @@ with open(filename, "w", newline='') as file:
     
     # Write CSV Headers
     file_writer.writerow([
-        "Time",
-        "Temp", "Gas", "Humidity", "Pressure", "Altitude",
-        "PM1.0std", "PM2.5std", "PM10std",
-        "PM1.0env", "PM2.5env", "PM10env",
-        "Particles 0.3um", "Particles 0.5um", "Particles 1.0um",
-        "Particles 2.5um", "Particles 5.0um", "Particles 10um"
+        "Current Time",
+        "Temperature (C)", "Gas (ohm)", "Humidity (%)", "Pressure (hPa)", "Altitude (m)",
+        "PM1.0 (standard)", "PM2.5 (standard)", "PM10 (standard)",
+        "PM1.0 (env)", "PM2.5 (env)", "PM10 (env)",
+        "Particles >0.3um", "Particles >0.5um", "Particles >1.0um",
+        "Particles >2.5um", "Particles >5.0um", "Particles >10um"
     ])
 
-    print("Reading data...")
+    print("reading data...")
     
     start_time = time.time()
     
     while time.time() - start_time < run_time:
         current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-        temp = bme680.temperature
+        temperature = bme680.temperature
         gas = bme680.gas
         humidity = bme680.relative_humidity
         pressure = bme680.pressure
@@ -57,19 +61,20 @@ with open(filename, "w", newline='') as file:
             aqdata = pm25.read()
         except RuntimeError:
             print("Unable to read from sensor, retrying...")
-            time.sleep(1)  # Wait before retrying
             continue
 
+      
         # Extract data and write to CSV
         row = [
             current_time,
-            temp, gas, humidity, pressure, altitude,
+            temperature, gas, humidity, pressure, altitude,
             aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"],
             aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"],
-            aqdata["particles 0.3um"], aqdata["particles 0.5um"], aqdata["particles 1.0um"],
-            aqdata["particles 2.5um"], aqdata["particles 5.0um"], aqdata["particles 10um"]
+            aqdata["particles 03um"], aqdata["particles 05um"], aqdata["particles 10um"],
+            aqdata["particles 25um"], aqdata["particles 50um"], aqdata["particles 100um"]
         ]
         file_writer.writerow(row)
         print("Logged data:", row)
 
-        time.sleep(2)  # Wait 2 seconds before the next reading
+
+   # time.sleep(2)  # Wait 2 seconds before next reading
